d9f04af00419e636e7c08eb2b7354f59c57cfbf4
Your Name <you@example.com>
2021-05-08 14:05:58 -0800

    first commit

---
+++ src/component/Shop/Shop.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import fakeData from '../../fakeData';
+import Cart from '../Cart/Cart';
+import Product from '../Product/Product';
+import './Shop.css';
+
+const Shop = () => {
+    // console.log(fakeData)
+    const frist10 = fakeData.slice(0, 10)
+    const [products, setProducts] = useState(frist10);
+
+    const [cart, setCart] = useState([])
+
+    const handleAddProduct = (product) => {
+        console.log("hello", product)
+        const newCart = [...cart, product]
+        setCart(newCart)
+    }
+    return (
+        <div className="shop-container">
+            
+           <div className="product-container">
+                {/* <h3>{products.length}</h3> */}
+                {
+                    products.map(pd => <Product
+                        handleAddProduct = {handleAddProduct}
+                        product={pd}>  
+                        </Product>)
+                }
+           </div>
+           <div className="cart-container">
+               <Cart cart={cart}></Cart>
+           </div>
+        </div>
+    );
+};
+
+export default Shop;
